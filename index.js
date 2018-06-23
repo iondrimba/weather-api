e22a7ac0cd955641798a2d5b8ca433b815236736
Ion D. Filho <iondrimba@gmail.com>
2018-06-22 21:13:36 -0300

    fixing fetch

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ app.get('/api', function(req, res) {
 const getWeatherCondition = async (url) => {
   try {
     const response = await fetch(url);
+    const result = await response.json();
 
-    return await response.json();
+    return result;
   } catch (error) {
     return error.message;
   }
