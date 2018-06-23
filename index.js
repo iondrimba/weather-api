f174b884db550ffe56e1ff2b72134dc4aa08723e
Ion D. Filho <iondrimba@gmail.com>
2018-06-22 21:29:01 -0300

    enable basic cors

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
   const data = {
