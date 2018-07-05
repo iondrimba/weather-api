9038933e5470c1ded3e304e4655bd673fc784df4
Ion D. Filho <iondrimba@gmail.com>
2018-07-04 22:01:23 -0300

    added compression

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const fetch = require("node-fetch");
+const compression = require('compression');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
