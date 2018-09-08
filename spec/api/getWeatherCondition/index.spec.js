45998ef5c147f522dbfd816ea675c364100c5a86
Ion D. Filho <iondrimba@gmail.com>
2018-09-08 17:23:48 -0300

    added getWeatherCondition specs

---
+++ spec/api/getWeatherCondition/index.spec.js
@@ -0,0 +1,26 @@
+const getWeatherCondition = require('../../../api/getWeatherCondition');
+const mockdataSucess = require('./mockForecast.json');
+
+describe('getWeatherCondition', () => {
+  beforeEach(() => {
+    require('dotenv').config();
+  });
+
+  it('returns forecast', async () => {
+    const secret = process.env.DARK_SKY_API_CODE;
+    const baseUrl = 'https://api.darksky.net/forecast';
+    const endpoint = (latitude, longitude) => `${baseUrl}/${secret}/${latitude},${longitude}?units=auto`;
+    const result = await getWeatherCondition(endpoint(-23.5733, -46.6417));
+
+    expect(result.latitude).toEqual(mockdataSucess.latitude);
+    expect(result.longitude).toEqual(mockdataSucess.longitude);
+    expect(result.timezone).toEqual(mockdataSucess.timezone);
+  });
+
+  it('returns exception message', async () => {
+    const url = 'xpto';
+    const result = await getWeatherCondition(url);
+
+    expect(result).toEqual('Only absolute URLs are supported');
+  });
+});
