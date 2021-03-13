6126cf99d265c8b2aa51b34567d51783cc85f94e
Ion Drimba Filho <iondrimba@users.noreply.github.com>
2021-03-13 15:27:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://weather.iondrimbafilho.me');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   next();
