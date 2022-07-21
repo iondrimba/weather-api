a989ead709f806436a66618c20a37b8855cf86b4
Ion Drimba Filho <iondrimba@users.noreply.github.com>
2022-07-21 12:00:24 -0300

    Update routes/ip.js

--- routes/ip.js
+++ routes/ip.js
@@ -2,7 +2,7 @@ const getGeolocationByIp = require('../api/getGeolocationByIp');
 
 module.exports = async (req, res) => {
   const endpoint = (ip) => `https://tools.keycdn.com/geo.json?host=${ip}`;
-  const data = await getGeolocationByIp(endpoint(req.query.ip), 'keycdn-tools:iondrimbafilho.me');
+  const data = await getGeolocationByIp(endpoint(req.query.ip), 'keycdn-tools:https://weather.iondrimbafilho.me');
 
   res.status(200).send(data);
 };
