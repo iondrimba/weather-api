9a9aceaeaad1c824b8a1d4f08d9bc9fbf25b589f
Ion Drimba Filho <iondrimba@users.noreply.github.com>
2025-05-01 18:07:33 -0300

    update endpoint (#250)

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,8 @@ const getWeatherCondition = require('../api/getWeatherCondition');
 module.exports = async (req, res) => {
   const secret = process.env.WEATHER_API;
   const baseUrl = 'https://api.weatherapi.com';
-  const endpoint = (ip) => `${baseUrl}/v1/current.json?q=${ip}&lang=pt&key=${secret}`;
+  const endpoint = (ip) => `${baseUrl}/v1/forecast.json?q=${ip}&lang=en&key=${secret}&days=6&aqi=no&alerts=no`;
+
   const data = await getWeatherCondition(endpoint(req.query.ip));
 
   res.status(200).send(data);
